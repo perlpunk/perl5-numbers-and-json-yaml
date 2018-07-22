f54d36178d2c55dfd58b90958b26458e408cda70
Tina Müller <cpan2@tinita.de>
2018-07-22 17:00:31 +0200

    Add HTML; add testcase

--- numbers.pl
+++ numbers.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
+use FindBin '$Bin';
 use Getopt::Long;
 use JSON::PP ();
 use JSON::XS ();
@@ -17,6 +18,7 @@ use YAML::XS ();
 use YAML::PP ();
 use Text::Table;
 use Devel::Peek ();
+use HTML::Template::Compiled;
 
 GetOptions(
     "format=s" => \my $format,
@@ -93,6 +95,11 @@ $x = 24.1;
 $x .= '';
 $x
 EOM
+    <<'EOM',
+$x = "25.0";
+$y = $x + 0;
+$x
+EOM
     q{$x = 0 + "inf"},
     q{$x = 0 - "inf"},
     q{$x = 0 + "nan"},
@@ -111,15 +118,30 @@ for my $code (@codes) {
 my @json = qw/ JSON::PP JSON::XS Cpanel::JSON::XS JSON::Tiny Mojo::JSON /;
 my @yaml = qw/ YAML YAML::XS YAML::Syck YAML::PP /;
 
-my @headers = ('', '', qw/ INT FLOAT STRING /, @json, @yaml);
+my @headers;
+my @versions;
 my @rows;
+if ($format eq 'html') {
+    @headers = ('', '', qw/ INT FLOAT STRING /, @json, @yaml);
+    @versions = ('','','','','');
+}
+else {
+    @headers = ('', '', qw/ INT FLOAT STRING /, @json, @yaml);
+    @versions = ('','','','','');
+}
 my $i = 0;
-my @versions = ('','','','','');
 for my $fw (@json, @yaml) {
     my $version = $fw eq 'Mojo::JSON' ? Mojolicious->VERSION: $fw->VERSION;
     push @versions, $version;
 }
-push @rows, \@versions;
+if ($format eq 'html') {
+    push @rows, [map {
+        +{ type => (length $? ? 'version' : ''), value => $_ }
+    } @versions];
+}
+else {
+    push @rows, \@versions;
+}
 
 
 for my $i (0 .. $#data) {
@@ -127,30 +149,50 @@ for my $i (0 .. $#data) {
     my $code = $codes[ $i ] // '-';
 #    Devel::Peek::Dump $item;
     my $flags = B::svref_2object(\$item)->FLAGS;
-    my $intflag = $flags & B::SVp_IOK ? 1 : '';
-    my $floatflag = $flags & B::SVp_NOK ? 1 : '';
-    my $stringflag = $flags & B::SVp_POK ? 1 : '';
+    my $intflag = $flags & B::SVp_IOK ? 'x' : '';
+    my $floatflag = $flags & B::SVp_NOK ? 'x' : '';
+    my $stringflag = $flags & B::SVp_POK ? 'x' : '';
 
     my @row;
-    push @row, $i++;
-    push @row, $code;
-    push @row, $intflag, $floatflag, $stringflag;
+    $i++;
+    if ($format eq 'html') {
+        push @row, { type => 'index', value => $i };
+        push @row, { type => 'code', value => $code };
+        push @row, { type => "flag$intflag", value => $intflag };
+        push @row, { type => "flag$floatflag", value => $floatflag };
+        push @row, { type => "flag$stringflag", value => $stringflag };
+    }
+    else {
+        push @row, $i;
+        push @row, $code;
+        push @row, $intflag, $floatflag, $stringflag;
+    }
     for my $fw (@json) {
         my $can = $fw->can("encode_json");
         my $json = $can->([$item]);
         my $version = $fw eq 'Mojo::JSON' ? Mojolicious->VERSION: $fw->VERSION;
         $json =~ s/^\[(.*)\]/$1/;
-        push @row, $json;
+        if ($format eq 'html') {
+            push @row, { type => 'json', value => $json };
+        }
+        else {
+            push @row, $json;
+        }
     }
     for my $fw (@yaml) {
         my $can = $fw->can("Dump");
         my $yaml = $can->($item);
         my $version = $fw->VERSION;
         $yaml =~ s/^---\s//;
-        push @row, $yaml;
+        if ($format eq 'html') {
+            push @row, { type => 'yaml', value => $yaml };
+        }
+        else {
+            push @row, $yaml;
+        }
     }
     push @rows, \@row;
-    push @rows, [(' ') x @row];
+    push @rows, [(' ') x @row] unless $format eq 'html';
 
 }
 
@@ -166,4 +208,17 @@ elsif ($format eq 'markdown') {
         s/\n/<br>/g for @$row;
         say '|', (join '|', @$row), '|';
     }
+}
+elsif ($format eq 'html') {
+    my $htc = HTML::Template::Compiled->new(
+        paths => "$Bin",
+        filename => "table.html",
+        tagstyle => [ qw/ -asp -comment +tt / ],
+        loop_context_vars => 1,
+    );
+    $htc->param(
+        header => \@headers,
+        rows => \@rows,
+    );
+    print $htc->output;
 }
